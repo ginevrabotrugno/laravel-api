e4b426a7233e99ddcdd5e986978df5fe481d3d59
ginevrabotrugno <ginevrabotrugno.gb@gmail.com>
2024-09-30 14:48:38 +0200

    feat: add api controller

---
+++ app/Http/Controllers/Api/PageController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+
+// API da realizzare:
+//  - elenco di tutti i progetti con associato il tipo e le tecnologie (con o senza paginazione)
+//  - elenco di tutte le tecnologie
+//  - elenco di tutti i tipi
+
+{
+    public function index(){
+        dd('API');
+    }
+}
