44b80c881bb7435776cee7a307e32ba9558c6ef9
ginevrabotrugno <ginevrabotrugno.gb@gmail.com>
2024-10-05 18:20:02 +0200

    feat: send mail

--- app/Http/Controllers/Api/LeadController.php
+++ app/Http/Controllers/Api/LeadController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Mail\NewContact;
+use App\Models\Lead;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Validator;
 
 class LeadController extends Controller
@@ -37,6 +40,12 @@ class LeadController extends Controller
             return response()->json(compact('success', 'errors'));
         }
 
-        return response()->json(compact('success', 'data'));
+        $new_lead = new Lead();
+        $new_lead->fill($data);
+        $new_lead->save();
+
+        Mail::to($new_lead->email)->send(new NewContact($new_lead));
+
+        return response()->json(compact('success'));
     }
 }
